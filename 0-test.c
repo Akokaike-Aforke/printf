5649d22a445a6c9597d09cc0eef669eb09f56696
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-10-16 03:49:48 -0700

    0-test edit

--- 0-test.c
+++ 0-test.c
@@ -8,14 +8,13 @@
 
 int _printf(const char *format, ...)
 {
-	int len1, i, j = 0;
+	int i, j = 0;
 	va_list spec;
-	char *copy;
+	char *copy, *newWord;
 
 	va_start(spec, format);
-	len1 = strlen(format);
-	copy = (char *)malloc(len1 + 1);
-	for (i = 0; i < len1; i++)
+	copy = (char *)malloc(strlen(format) + 1);
+	for (i = 0; i < strlen(format); i++)
 	{
 		if (format[i] != '%')
 		{
@@ -30,10 +29,24 @@ int _printf(const char *format, ...)
 				copy[j] = '%';
 				j++;
 			}
+			else if (format[i] == 'c')
+			{
+				copy[j] = va_arg(spec, int);
+				j++;
+			}
+			else if (format[i] == 's')
+			{
+				copy[j] = '\0';
+				newWord = va_arg(spec, char*);
+				copy = realloc(copy, strlen(format) + strlen(newWord));
+				strcat(copy, newWord);
+				j += strlen(newWord);
+			}
 		}
 	}
 	copy[j] = '\0';
 	va_end(spec);
 	write(1, copy, j);
+	free(copy);
 	return (strlen(copy));
 }
