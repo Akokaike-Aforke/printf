86149e30106e42c801abb06529ebcf7d36f50c65
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-10-16 19:42:36 +0100

    update

--- itoa.c
+++ itoa.c
@@ -10,27 +10,23 @@ char *itoa(int num, char *str, int base) {
         return str;
     }
 
-    // Handle negative numbers for bases other than 10
     if (num < 0 && base == 10) {
         isNegative = 1;
         num = -num;
     }
 
-    // Convert the number to a string in reverse order
     while (num != 0) {
         int rem = num % base;
         str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
         num /= base;
     }
 
-    // Add the negative sign for base 10
     if (isNegative && base == 10) {
         str[i++] = '-';
     }
 
     str[i] = '\0';
 
-    // Reverse the string
     int start = 0;
     int end = i - 1;
     while (start < end) {
