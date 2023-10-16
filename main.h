00ea282e859eb1a1a88fdef700fcb65374e3c93a
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-10-16 16:47:14 -0700

    added extra function

--- main.h
+++ main.h
@@ -4,10 +4,12 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
 void getcharacter(char *copy, char c, unsigned int *j);
 void get_word_num(char *copy, char *word, unsigned int *j);
 char *itoa(int num, char *str, int base);
+char *get_other(char c);
 
-#endif
+#endif /* MAIN_H */
