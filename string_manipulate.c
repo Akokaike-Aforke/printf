39d65279c4dfcbe84bfcf889fc0cc778c495a391
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-10-17 07:07:11 -0700

    edited strev

--- string_manipulate.c
+++ string_manipulate.c
@@ -1,46 +1,28 @@
 #include "main.h"
-<<<<<<< HEAD
 /**
- * strrev - reverses a string
+ * my_strrev - reverses a string
  * @word: string to be reversed
  * Return: a string
  */
-char *strrev(char *word)
+char *my_strrev(char *word)
 {
 	int i;
 	int j;
-	char *rev = (char *)malloc(10);
+	char *rev;
 
 	j = 0;
-
+	rev = (char *)malloc(sizeof(char) * j + 1);
+	if (rev == NULL)
+	{
+		perror("Memory allocation failed");
+		exit(1);
+	}
 	for (i = strlen(word) - 1; i >= 0; i--)
 	{
 		rev[j] = '\0' + word[i];
 		j++;
+		rev = realloc(rev, sizeof(char) * j + 1);
 	}
 	rev[j] = '\0';
 	return (rev);
-=======
-char *my_strrev(char *word)
-{
-    int i;
-    int j;
-    j = 0;
-    char *rev = (char*)malloc(sizeof(char) * j + 1);
-    if(rev == NULL)
-    {
-        perror("Memory allocation failed");
-        exit(1);
-
-    }
-    
-    for (i = strlen(word) - 1; i >= 0; i--)
-    {
-        rev[j] = '\0' + word[i];
-        j++;
-        rev = realloc(rev, sizeof(char) * j + 1);
-    }
-    rev[j] = '\0';
-    return rev;
->>>>>>> 96829319c2101b16ecf93be222c0b6f5a8433295
 }
