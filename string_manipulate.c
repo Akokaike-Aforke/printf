e2af5e93d805a38d6519ab03b24aa199c64c2258
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-10-17 23:55:00 +0100

    hexadecimal specifier

--- string_manipulate.c
+++ string_manipulate.c
@@ -26,3 +26,49 @@ char *my_strrev(char *word)
 	rev[j] = '\0';
 	return (rev);
 }
+char *get_hex(unsigned int dec_num)
+{
+    unsigned int i;
+     int j;
+     j = 0;
+    char * num = (char *) malloc(sizeof(char) * j + 1);
+     if(num == NULL)
+    {
+        perror("Memory allocation failed");
+        exit(1);
+
+    }
+    char *final_num;
+    for (i = dec_num; i > 0; i/=16)
+    {
+        if(i % 16 == 10)
+        num[j] = 'A';
+        else if(i % 16 == 11)
+        num[j] = 'B';
+        else if(i % 16 == 12)
+        num[j] = 'C';
+        else if(i % 16 == 13)
+        num[j] = 'D';
+        else if(i % 16 == 14)
+        num[j] = 'E';
+        else if(i % 16 == 15)
+        num[j] = 'F';
+        else
+        num[j] = '0' + (i % 16);
+        j++;
+        num = realloc(num, sizeof(char) * j + 1);
+    }
+    num[j] = '\0';
+
+    final_num = (char *) malloc(sizeof(num)* j + 1);
+     if(final_num == NULL)
+    {
+        perror("Memory allocation failed");
+        exit(1);
+
+    }
+    final_num = my_strrev(num);
+    free(num);
+    return final_num;
+
+}
